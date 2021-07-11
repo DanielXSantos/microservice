b2e11ae6ebb5fc5100d4700254c2317379fefb20
Daniel Santos <daniel.victor@tqi.com.br>
2021-07-11 19:24:10 -0300

    feignclient implementation; insert worker

--- hr-payroll/src/main/java/com/ms/hrpayroll/service/PaymentService.java
+++ hr-payroll/src/main/java/com/ms/hrpayroll/service/PaymentService.java
@@ -2,6 +2,7 @@ package com.ms.hrpayroll.service;
 
 import com.ms.hrpayroll.entities.Payment;
 import com.ms.hrpayroll.entities.Worker;
+import com.ms.hrpayroll.feignclients.WorkerFeignClient;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,19 +13,13 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerBaseUrl;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days) {
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", ""+workerId);
-
-        Worker worker = restTemplate
-                .getForObject(workerBaseUrl+"/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
+
     }
 
 }
--- hr-worker/src/main/java/com/ms/hrworker/controller/WorkerController.java
+++ hr-worker/src/main/java/com/ms/hrworker/controller/WorkerController.java
@@ -3,10 +3,13 @@ package com.ms.hrworker.controller;
 import com.ms.hrworker.entities.Worker;
 import com.ms.hrworker.repository.WorkerRepository;
 import java.util.List;
+import javax.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +29,12 @@ public class WorkerController {
     @GetMapping(value = "/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
         Worker worker = workerRepository.findById(id).get();
+        return ResponseEntity.ok(worker);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Worker> insertWorker(@RequestBody Worker worker) {
+        workerRepository.save(worker);
         return ResponseEntity.ok(worker);
     }
 
