69922f36d11bcf05fe57013dcabd1f27956fd63e
Daniel Santos <daniel.victor@tqi.com.br>
2021-07-10 23:57:15 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_worker (name, daily_Income) VALUES ('lula', 200.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Marina', 300.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Temer', 250.0);
+INSERT INTO tb_worker (name, daily_Income) VALUES ('Bolso', 2555550.0);
