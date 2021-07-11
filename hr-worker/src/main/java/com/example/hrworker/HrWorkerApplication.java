0645bd15647728f34488efd87bc96b6b0cdbc03f
Daniel Santos <daniel.victor@tqi.com.br>
2021-07-10 21:28:32 -0300

    first commit

---
+++ hr-worker/src/main/java/com/example/hrworker/HrWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.example.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkerApplication {
+
+	public static void main(String[] args) throws InterruptedException {
+		SpringApplication.run(HrWorkerApplication.class, args);
+	}
+
+}
